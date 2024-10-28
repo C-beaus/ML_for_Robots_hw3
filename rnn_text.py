b3b976ef1ab6686a554c351946e826d61962876b
C-beaus <cabeausoleil@wpi.edu>
2024-10-27 20:56:04 -0400

    removed deprecated code

--- rnn_text.py
+++ rnn_text.py
@@ -14,14 +14,6 @@ import torchvision.transforms as transforms
 from torch.utils.tensorboard import SummaryWriter
 import csv
 from mpl_toolkits.mplot3d import Axes3D
-# import datasets
-
-
-# from torchtext.data import get_tokenizer
-# from torchtext.vocab import build_vocab_from_iterator
-
-# pytorch loads the IMBD data for you, but if you want to look at the raw format, you can download from here:
-# https://www.kaggle.com/datasets/lakshmi25npathi/imdb-dataset-of-50k-movie-reviews
 
 
 seed = 1234
@@ -150,34 +142,20 @@ class Dubin_Path_RNN(nn.Module):
             dropout=dropout_rate,
             batch_first=True
         )
-        # self.lstm = nn.LSTM(
-        #     input_dim,
-        #     hidden_dim, # hidden_dim
-        #     n_layers,
-        #     bidirectional=bidirectional,
-        #     dropout=dropout_rate,
-        #     batch_first=True
-        # )
-
-        # self.output_layer = nn.Linear(hidden_dim * 2 if bidirectional else hidden_dim, output_dim)
         self.output_layer = nn.Sequential(
              nn.Linear(hidden_dim, 256),
              nn.ReLU(),
              nn.Linear(256, output_dim)
         )
 
-        # self.output_layer = nn.Linear(hidden_dim * 2 if bidirectional else output_dim, output_dim)
         self.dropout = nn.Dropout(dropout_rate)
 
     def forward(self, initial_conditions, ground_truth, teacher_forcing_prob, hidden_in):
        
        batch_size = initial_conditions.size(0)
        lstm_input = self.fc(initial_conditions).unsqueeze(1)
-    #    cell = torch.zeros_like(hidden)
 
        outputs = []
-    #    lstm_input = torch.zeros(batch_size, 1, hidden.size(-1))
-    #    lstm_input = hidden
 
        for i in range(ground_truth.size(1)):
            out, hidden_in = self.lstm(lstm_input, hidden_in)
@@ -189,30 +167,8 @@ class Dubin_Path_RNN(nn.Module):
                 lstm_input = ground_truth[:, i].unsqueeze(1)
            else:
                 lstm_input = trajectory_point.unsqueeze(1)
-        #    lstm_input = out
        return torch.cat(outputs, dim=1)
     
-
-
-
-        # embedded = self.dropout(self.embedding(ids))
-        # packed_embedded = nn.utils.rnn.pack_padded_sequence(
-        #     embedded, length, batch_first=True, enforce_sorted=False
-        # ) # makes padding ignored by RNN
-        # packed_output, (hidden, cell) = self.lstm(packed_embedded)
-        # # hidden = [n layers * n directions, batch size, hidden dim]
-        # # cell = [n layers * n directions, batch size, hidden dim]
-        # output, output_length = nn.utils.rnn.pad_packed_sequence(packed_output)
-        # # output = [batch size, seq len, hidden dim * n directions]
-        # if self.lstm.bidirectional:
-        #     hidden = self.dropout(torch.cat([hidden[-1], hidden[-2]], dim=-1))
-        #     # hidden = [batch size, hidden dim * 2]
-        # else:
-        #     hidden = self.dropout(hidden[-1])
-        #     # hidden = [batch size, hidden dim]
-        # prediction = self.fc(hidden)
-        # # prediction = [batch size, output dim]
-        # return prediction
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -274,10 +230,6 @@ if __name__ == '__main__':
     # remove initial position, steplength, and r_min from data
     parameters_data = parameters_data[:, 3:7]
 
-
-    # temp_params = torch.tensor([], dtype=torch.float32).to(device)
-    # temp_psi_end = torch.tensor([], dtype=torch.float32).to(device)
-
     params_counter = 0
     psi_end_counter = 0
 
@@ -298,15 +250,6 @@ if __name__ == '__main__':
         psi_end_counter += 1
 
     psi_end_data = temp_psi_end
-    
-    
-
-    # List to store the data from all files
-    # dataframes = []
-    # paths = []
-    # paths = torch.tensor(paths, dtype=torch.float32).to(device)
-
-    # paths = np.zeros((10000, 1000, 3)) # depth, rows, columns
     paths = np.zeros((10000, 175, 3))
 
     df = sio.loadmat(data_path)
@@ -314,17 +257,12 @@ if __name__ == '__main__':
     # Loop through all files in the folder
     for i in range(df.shape[2]):
         if i == 0:
-            # paths = torch.tensor(df[:, :, i], dtype=torch.float32).to(device)
             paths[i, :, :] = df[0:175, :, i]
         else:
-            # paths = torch.vstack((paths, torch.tensor(df[:,:,i], dtype=torch.float32).to(device)))
-            # paths[:, :, i] = torch.tensor(df[:, :, i], dtype = torch.float32).to(device)
             paths[i, :, :] = df[0:175, :, i]
     paths = torch.tensor(paths, dtype=torch.float32).to(device)
-    # paths = torch.stack(paths, dim=2)
     
     train_indices = int(((i + 1) * 0.8) - 1)
-    # val_indices = int(((i + 1) * 0.2) - 1)
 
     train_paths = paths[0:train_indices, :, :]
     val_paths = paths[train_indices + 1:-1, :, :]
@@ -344,7 +282,6 @@ if __name__ == '__main__':
     train_dataset = TrajectoryDataset(train_params, train_paths, train_params_mean, train_params_std, train_paths_mean, train_paths_std)
     train_loader = DataLoader(train_dataset, batch_size = 32, shuffle = True)
 
-
     val_params_mean = val_params.mean(dim=0)
     val_params_std = val_params.std(dim=0)
 
@@ -354,11 +291,9 @@ if __name__ == '__main__':
     val_dataset = TrajectoryDataset(val_params, val_paths, val_params_mean, val_params_std, val_paths_mean, val_paths_std)
     val_loader = DataLoader(val_dataset, batch_size = val_paths.size(0), shuffle = True)
 
-
     # Model Params
     input_size = parameters_data.shape[1]
     hidden_dim = 128 #dimension of the hidden state
-    # lstm_hidden_dim = 128
     output_dim = 3
     n_layers = 2
     bidirectional = False
@@ -405,10 +340,6 @@ if __name__ == '__main__':
                     torch.zeros(n_layers, batch_size, hidden_dim))
                 predictions = model(train_params, train_paths, teacher_forcing_prob, hidden_in)
 
-                # # Reinstate 
-                # predictions = predictions * train_paths_std + train_paths_mean
-                # train_paths = train_paths * train_paths_std + train_paths_mean
-
                 # Compute train loss
                 loss = criterion(predictions, train_paths)
                 loss.backward()
@@ -431,13 +362,13 @@ if __name__ == '__main__':
                     fig = plt.figure(figsize=(10, 7))
                     ax = fig.add_subplot(111, projection='3d')
 
-                    x_pred = predictions_val[d, :, 0].detach().numpy()  # Feature 1
-                    y_pred = predictions_val[d, :, 1].detach().numpy()  # Feature 2
-                    z_pred = predictions_val[d, :, 2].detach().numpy()  # Feature 3
+                    x_pred = predictions_val[d, :, 0].detach().numpy()  
+                    y_pred = predictions_val[d, :, 1].detach().numpy()  
+                    z_pred = predictions_val[d, :, 2].detach().numpy()  
 
-                    x_gtruth = val_paths[d, :, 0].numpy()  # Feature 1
-                    y_gtruth = val_paths[d, :, 1].numpy()  # Feature 2
-                    z_gtruth = val_paths[d, :, 2].numpy()  # Feature 3
+                    x_gtruth = val_paths[d, :, 0].numpy()  
+                    y_gtruth = val_paths[d, :, 1].numpy()  
+                    z_gtruth = val_paths[d, :, 2].numpy()  
 
                     ax.plot(x_pred, y_pred, z_pred, label=f'Prediction, Depth {d+1}', marker='o')
                     ax.plot(x_gtruth, y_gtruth, z_gtruth, label=f'Ground Truth, Depth {d+1}', marker='_')
@@ -454,13 +385,6 @@ if __name__ == '__main__':
         
                 
         csv_writer.writerows([predictions[1, :, :].detach().numpy(),train_paths[1, :, :].numpy(), predictions_val[1,:,:].detach().numpy(),val_paths[1,:,:].numpy()])
-
-    # # Create image folder
-    # # Will's Path
-    # # folder_path = "C:/Users/willi/Documents/GitHub/ML_for_Robots_hw3/images"
-    # # Chase's Path
-    # folder_path = 'c:/Users/chase/OneDrive/Documents/Grad/ML_for_Robots/hw_3/ML_for_Robots_hw3/images'
-    # os.makedirs(folder_path, exist_ok=True)
 
     # Create a 3D plot
     for d in range(10):
@@ -488,179 +412,3 @@ if __name__ == '__main__':
         plt.legend()
         plt.savefig(img_path)
     plt.show()
-
-    # # Loop through each depth level and plot the sequence and features
-    # for d in range(10):
-    #     x_pred = predictions[d, :, 0].detach().numpy()  # Feature 1
-    #     y_pred = predictions[d, :, 1].detach().numpy()  # Feature 2
-    #     z_pred = predictions[d, :, 2].detach().numpy()  # Feature 3
-
-    #     x_gtruth = train_paths[d, :, 0].numpy()  # Feature 1
-    #     y_gtruth = train_paths[d, :, 1].numpy()  # Feature 2
-    #     z_gtruth = train_paths[d, :, 2].numpy()  # Feature 3
-
-    #     ax.plot(x_pred, y_pred, z_pred, label=f'Prediction, Depth {d+1}', marker='o')
-    #     ax.plot(x_gtruth, y_gtruth, z_gtruth, label=f'Ground Truth, Depth {d+1}', marker='_')
-
-    # img_path = 'test_img.png'
-
-    # # Set labels
-    # ax.set_xlabel('X Axis')
-    # ax.set_ylabel('Y Axis')
-    # ax.set_zlabel('Z Axis')
-    # plt.title('3D Plot of Trajectory Data')
-    # plt.legend()
-    # plt.savefig(img_path)
-    
-    # plt.show()
-
-    # # Normalization of paths (We don't need)
-    # # normalized_train_paths = (train_paths - torch.mean(train_paths))/torch.std(train_paths)
-    # # normalized_val_paths = (val_paths - torch.mean(val_paths))/torch.std(val_paths)
-
-
-    # # train_data, test_data = datasets.load_dataset("imdb", split=["train", "test"])
-    # # # tokenizer = get_tokenizer("basic_english")
-
-
-    # # train_data = train_data.map(
-    # #     tokenize_example, fn_kwargs={"tokenizer": tokenizer, "max_length": max_length}
-    # # )
-    # # test_data = test_data.map(
-    # #     tokenize_example, fn_kwargs={"tokenizer": tokenizer, "max_length": max_length}
-    # # )
-
-
-
-    # # test_size = 0.25
-
-    # # train_valid_data = train_data.train_test_split(test_size=test_size)
-    # # train_data = train_valid_data["train"]
-    # # valid_data = train_valid_data["test"]
-
-
-
-    # # min_freq = 5
-    # # special_tokens = ["<unk>", "<pad>"]
-
-    # # # vocab = build_vocab_from_iterator(
-    # # #     train_data["tokens"],
-    # # #     min_freq=min_freq,
-    # # #     specials=special_tokens,
-    # # # )
-
-
-    # # unk_index = vocab["<unk>"]
-    # pad_index = 0
-
-    # # # any token not found in the vocabulary will be mapped to the <unk> token.
-    # # vocab.set_default_index(unk_index)
-    # # # converting each example's tokens to their corresponding numerical indices using the provided vocabulary.
-    # # train_data = train_data.map(numericalize_example, fn_kwargs={"vocab": vocab})
-    # # valid_data = valid_data.map(numericalize_example, fn_kwargs={"vocab": vocab})
-    # # test_data = test_data.map(numericalize_example, fn_kwargs={"vocab": vocab})
-    # # # sets the format of the datasets to PyTorch tensors. It specifies that the columns to be converted to tensors are ids, label, and length
-    # # train_data = train_data.with_format(type="torch", columns=["ids", "label", "length"])
-    # # valid_data = valid_data.with_format(type="torch", columns=["ids", "label", "length"])
-    # # test_data = test_data.with_format(type="torch", columns=["ids", "label", "length"])
-
-
-    # # train_data[0]
-
-    # batch_size = 512
-
-    # train_data_loader = get_data_loader(train_paths, batch_size, pad_index, shuffle=True)
-    # valid_data_loader = get_data_loader(val_paths, batch_size, pad_index)
-    # test_data_loader = get_data_loader(val_paths, batch_size, pad_index)
-
-    # # Model Params
-    # input_size = parameters_data.shape[1]
-    # hidden_dim = 250 #dimension of the hidden state
-    # output_dim = 3
-    # n_layers = 2
-    # bidirectional = False
-    # dropout_rate = 0.5
-
-    # model = Multi_Layer_LSTM(input_size, hidden_dim, output_dim, n_layers, bidirectional, dropout_rate)
-
-    # # print(f"The model has {count_parameters(model):,} trainable parameters")
-
-    # model.apply(initialize_weights)
-    # # vectors = torchtext.vocab.GloVe()
-    # # pretrained_embedding = vectors.get_vecs_by_tokens(vocab.get_itos())
-    # # model.embedding.weight.data = pretrained_embedding
-    # lr = 5e-4
-    # optimizer = optim.Adam(model.parameters(), lr=lr)
-    # criterion = nn.CrossEntropyLoss()
-    # # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    # model = model.to(device)
-    # criterion = criterion.to(device)
-
-
-    # n_epochs = 10
-    # best_valid_loss = float("inf")
-
-    # metrics = collections.defaultdict(int)
-
-    # for epoch in range(n_epochs):
-    #     train_loss, train_acc = train(
-    #         train_data_loader, model, criterion, optimizer, device
-    #     )
-    #     valid_loss, valid_acc = evaluate(valid_data_loader, model, criterion, device)
-    #     metrics["train_losses"].append(train_loss)
-    #     metrics["train_accs"].append(train_acc)
-    #     metrics["valid_losses"].append(valid_loss)
-    #     metrics["valid_accs"].append(valid_acc)
-    #     if valid_loss < best_valid_loss:
-    #         best_valid_loss = valid_loss
-    #         torch.save(model.state_dict(), "lstm.pt")
-    #     print(f"epoch: {epoch}")
-    #     print(f"train_loss: {train_loss:.3f}, train_acc: {train_acc:.3f}")
-    #     print(f"valid_loss: {valid_loss:.3f}, valid_acc: {valid_acc:.3f}")
-
-
-
-
-    # fig = plt.figure(figsize=(10, 6))
-    # ax = fig.add_subplot(1, 1, 1)
-    # ax.plot(metrics["train_losses"], label="train loss")
-    # ax.plot(metrics["valid_losses"], label="valid loss")
-    # ax.set_xlabel("epoch")
-    # ax.set_ylabel("loss")
-    # ax.set_xticks(range(n_epochs))
-    # ax.legend()
-    # ax.grid()
-
-
-
-    # fig = plt.figure(figsize=(10, 6))
-    # ax = fig.add_subplot(1, 1, 1)
-    # ax.plot(metrics["train_accs"], label="train accuracy")
-    # ax.plot(metrics["valid_accs"], label="valid accuracy")
-    # ax.set_xlabel("epoch")
-    # ax.set_ylabel("loss")
-    # ax.set_xticks(range(n_epochs))
-    # ax.legend()
-    # ax.grid()
-
-    # model.load_state_dict(torch.load("lstm.pt"))
-
-    # test_loss, test_acc = evaluate(test_data_loader, model, criterion, device)
-
-    # print(f"test_loss: {test_loss:.3f}, test_acc: {test_acc:.3f}")
-
-    # # text = "This film is terrible!"
-
-    # # predict_sentiment(text, model, tokenizer, vocab, device)
-
-    # # text = "This film is great!"
-
-    # # predict_sentiment(text, model, tokenizer, vocab, device)
-    # # text = "This film is not terrible, it's great!"
-
-    # # predict_sentiment(text, model, tokenizer, vocab, device)
-
-    # # text = "This film is not great, it's terrible!"
-
-    # # predict_sentiment(text, model, tokenizer, vocab, device)
-
